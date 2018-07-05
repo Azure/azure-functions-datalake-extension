4a6994002ab727b17a253bee9b278826792ea21d
Paul Batum <paul.batum@gmail.com>
2018-07-05 11:37:26 -0700

    Add a comment to DataLakeStoreOutput as we need to think about this one a little more

--- src/WebJobs.Extensions.DataLake/DataLakeStoreOutput.cs
+++ src/WebJobs.Extensions.DataLake/DataLakeStoreOutput.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Microsoft.Azure.WebJobs.Extensions.DataLake
 {
+    /// <summary>
+    /// TO DO - figure out what to do about this type. Ideally the DataLake SDK would provide this type and we would not have
+    /// to define it ourselves.
+    /// </summary>
     public class DataLakeStoreOutput
     {
         /// <summary>
